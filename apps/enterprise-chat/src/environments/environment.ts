111df2e12fc526bf62bd7bf8321f9aa30e39c7f0
CK Sun <fgriasa123@gmail.com>
2026-03-14 01:47:53 -0700

    fix: add production Cognito config to chat app environment

--- apps/enterprise-chat/src/environments/environment.ts
+++ apps/enterprise-chat/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   isProduction: true,
   cognito: {
-    userPoolId: '',
-    clientId: '',
+    userPoolId: 'us-west-1_lxON3IDgv',
+    clientId: '3jkm59nrob2el204otm7qlv8qu',
   },
   teamclawGatewayUrl: '',
   adminApiUrl: 'https://9qt1dgv5d5.execute-api.us-west-1.amazonaws.com',
